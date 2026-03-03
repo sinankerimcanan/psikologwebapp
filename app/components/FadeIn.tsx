58043827a81e924969ef50c91174df7ded541714
sinankerimcanan <sinankerim2016@gmail.com>
2026-03-03 12:31:05 +0300

    refactor: simplify initial state calculations and update viewport margin in FadeIn component

--- app/components/FadeIn.tsx
+++ app/components/FadeIn.tsx
@@ -22,25 +22,15 @@ export default function FadeIn({
 }: FadeInProps) {
   const initial = {
     opacity: 0,
-    x:
-      direction === "left"
-        ? distance
-        : direction === "right"
-          ? -distance
-          : 0,
-    y:
-      direction === "up"
-        ? distance
-        : direction === "down"
-          ? -distance
-          : 0,
+    x: direction === "left" ? distance : direction === "right" ? -distance : 0,
+    y: direction === "up" ? distance : direction === "down" ? -distance : 0,
   };
 
   return (
     <motion.div
       initial={initial}
       whileInView={{ opacity: 1, x: 0, y: 0 }}
-      viewport={{ once: true, margin: "-60px" }}
+      viewport={{ once: true, margin: "0px" }}
       transition={{
         duration,
         delay,
